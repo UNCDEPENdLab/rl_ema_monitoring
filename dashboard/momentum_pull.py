2eabaca24542f514580dadad34b12774d63049c0
Shane Buckley <tshanebuckley@gmail.com>
2021-02-02 07:44:28 -0500

    added --path argument

--- dashboard/momentum_pull.py
+++ dashboard/momentum_pull.py
@@ -12,9 +12,11 @@ def create_arg_parser():
     Method for creating a parser for input arguments.
     '''
     # Initialize the argparser
-    parser = argparse.ArgumentParser(description='Tool for initializing a subject into the current project.')
+    parser = argparse.ArgumentParser(description='Tool for pulling subject data into a project.')
     # id argument
     parser.add_argument('--id', help='the subject id', default = None, required=True)
+    # id argument
+    parser.add_argument('--path', help='path the subject data directory', default = None, required=False)
     # gmail argument
     #parser.add_argument('--gmail', help='the gmail associated with the subjects phone and google drive', default = None, required=True)
     # return the parser object
@@ -197,6 +199,9 @@ def main():
     parsed_args = arg_parser.parse_args(sys.argv[1:])
     # The subject id
     id = parsed_args.id
+    # If the there is a path given, set as currDir
+    if parsed_args.path != None:
+        currDir = parsed_args.path
     # Pull the data and update the subject's json file
     pull_files(id=id, path=currDir)
 
