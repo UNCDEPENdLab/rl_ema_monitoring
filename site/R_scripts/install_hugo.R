3f00f0b406d78841bdd4ad84d99756d1bec5255e
Shane Buckley <shanebuckley@tolman.psych.pitt.edu>
2021-04-27 09:47:17 -0400

    styling update and daemon updates, beginning of network self-hosting

---
+++ site/R_scripts/install_hugo.R
@@ -0,0 +1,2 @@
+# install hugo via blogdown
+blogdown::install_hugo("0.82.0", extended=TRUE, force=TRUE)
--- site/R_scripts/stop_site.R
+++ site/R_scripts/stop_site.R
@@ -3,12 +3,15 @@
 # deactivate a daemon for auto-rendering
 # rmd files if their data changes.
 ################################
-
+library("future")
 # source the daemon_utils
 source("R_scripts/daemon_utils.R")
 
+# set the plan to not be sequential
+plan(multisession)
+
 # stop the rmd render daemon
-stopRMDDaemon()
+async_stopRMDDaemon <- future({stopRMDDaemon()})
 
 # stop the blogdown daemon
-startBlogdownDaemon()
+async_stopBlogdownDaemon <- future({stopBlogdownDaemon()})
