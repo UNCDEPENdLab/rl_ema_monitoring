c6bf8e61a035f1bfac8893f53422d23d8cf4db75
andrewpapale <papalae04@gmail.com>
2023-12-29 15:17:30 -0500

    wrapper function for TF

---
+++ TF_Scripts/wrap_load_epoch_TF_US.m
@@ -0,0 +1,14 @@
+% 2023-12-29 AndyP
+% Wrapper script for load_epoch_TF_US
+% Runs TF analysis on Momentum subjects using FieldTrip toolbox
+
+
+load('MomentumIDs226.mat','IDs');
+
+nD = length(IDs);
+
+parfor iD = 1:nD
+    name = IDs(iD,2);
+    disp(name);
+    load_epoch_TF_US(name);
+end
