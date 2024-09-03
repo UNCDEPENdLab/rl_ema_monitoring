ef6978dff94f691dc453c55a8a4e851dc2771d56
andrewpapale <papalae04@gmail.com>
2024-09-03 12:25:05 -0400

    Aug_24 EEG TF analysis should be ready

--- TF_Scripts/wrap_load_epoch_TF_US.m
+++ TF_Scripts/wrap_load_epoch_TF_US.m
@@ -2,19 +2,22 @@
 % Wrapper script for load_epoch_TF_US
 % Runs TF analysis on Momentum subjects using FieldTrip toolbox
 
-
+IDs_new = readmatrix('momentum308.csv');
 load('MomentumIDs226.mat','IDs');
-ID1 = IDs(88,:);
-ID2 = IDs(61,:);
 
-IDs = cat(1,ID1,ID2);
+IDs = setdiff(IDs_new(:,2), IDs(:,2));
+
+%ID1 = IDs(88,:);
+%ID2 = IDs(61,:);
+
+%IDs = cat(1,ID1,ID2);
 nD = length(IDs);
 
 
 ft_defaults
 
 parfor iD = 1:nD
-    name = mat2str(IDs(iD,2));
+    name = mat2str(IDs(iD));
     disp(name);
     load_epoch_TF_US(name);
 end
